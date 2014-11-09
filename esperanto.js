db58a8e582dbcc1bcbdf20b56fa001a01e4373bf
Gordon Guthrie <gordon@vixo.com>
2014-11-09 22:27:41 +0000

    added local storage and version

--- esperanto.js
+++ esperanto.js
@@ -10,36 +10,35 @@ ESP.supports_html5_storage = function () {
 
 ESP.sendRequest = function (url, callback, postData) {
     var req = ESP.createXMLHTTPObject();
-    if (!req) return;
+    if (!req) {
+        return;
+    };
     var method = (postData) ? "POST" : "GET";
+    req.overrideMimeType("application/json");
     req.open(method,url,true);
     req.setRequestHeader('User-Agent','XMLHTTP/1.0');
-    if (postData)
+    if (postData) {
         req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    };
     req.onreadystatechange = function () {
         if (req.readyState != 4) return;
         if (req.status != 200 && req.status != 304) {
-//          alert('HTTP error ' + req.status);
+            // alert('HTTP error ' + req.status);
             return;
         }
         callback(req);
     }
-    if (req.readyState == 4) return;
+    if (req.readyState == 4) {
+        return;
+    };
     req.send(postData);
 }
 
-ESP.XMLHttpFactories = [
-    function () {return new XMLHttpRequest()},
-    function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-    function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-    function () {return new ActiveXObject("Microsoft.XMLHTTP")}
-];
-
 ESP.createXMLHTTPObject = function() {
     var xmlhttp = false;
-    for (var i=0; i < ESP.XMLHttpFactories.length; i++) {
+    for (var i=0; i < ESP.data.XMLHttpFactories.length; i++) {
         try {
-            xmlhttp = ESP.XMLHttpFactories[i]();
+            xmlhttp = ESP.data.XMLHttpFactories[i]();
         }
         catch (e) {
             continue;
@@ -81,19 +80,53 @@ ESP.bind = function (event, id, fun) {
     var elem = document.getElementById(id).addEventListener(event, fun);
 };
 
-ESP.load_wordlist = function () {
-    var url, callback;
-    url = "./wordlist.json";
-    callback = function(wordlist) {
-        console.log(wordlist.response);
+ESP.get_version = function () {
+    var version;
+    version = document.getElementsByTagName("body")[0].getAttribute("data-version");
+    return version;
+}
+
+ESP.maybe_load_wordlist = function () {
+    var currentversion, storedversion, url, callback, key;
+    currentversion = ESP.get_version();
+    storedversion = localStorage.getItem("version");
+    if (currentversion !== storedversion) {
+        url = "./wordlist.json";
+        callback = function(wordlist) {
+            var words, i, esperanto, english, description, difficulty;
+            words = JSON.parse(wordlist.response)["wordlist"];
+            for (i = 0; i < words.length; i++) {
+                difficulty = words[i]["difficulty"];
+                if (! ESP.data.wordlists[difficulty]) {
+                    ESP.data.wordlists[difficulty] = [];
+                };
+                ESP.data.wordlists[difficulty].push(words[i]);
+            }
+            for (key in ESP.data.wordlists) {
+                localStorage.setItem(key, ESP.data.wordlists[key]);
+            };
+        };
+        return ESP.sendRequest(url, callback, false);
     };
-    return ESP.sendRequest(url, callback, false);
 };
 
+// ESP Data Structures
+
+ESP.data = {};
+
+ESP.data.XMLHttpFactories = [
+    function () {return new XMLHttpRequest()},
+    function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+    function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+    function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+];
+
+ESP.data.wordlists = {};
+
 ESP.main = function () {
-    var hasLocalStorage;
-    hasLocalStorage = ESP.supports_html5_storage();
-    if ( !hasLocalStorage) {
+    var LocalStorage;
+    LocalStorage = ESP.supports_html5_storage();
+    if ( !LocalStorage) {
         ESP.write_to_page("Esperanto Flashcards required local storage to work");
         return false;
     } else {
@@ -103,8 +136,8 @@ ESP.main = function () {
         ESP.bind("click", "wrong",      ESP.click_score);
         ESP.bind("click", "difficulty", ESP.select_difficulty);
         ESP.bind("click", "language",   ESP.select_language);
-        ESP.write_to_page("Yay");
-        ESP.load_wordlist();
+        // ESP.write_to_page(" ");
+        ESP.maybe_load_wordlist();
     };
 }
 
